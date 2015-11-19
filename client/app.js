19bdacd671ab837a80397b560afd3863512d0994
Ryan Tang <ryantang333@gmail.com>
2015-11-19 15:54:42 -0800

    modifying routes

--- client/app.js
+++ client/app.js
@@ -16,10 +16,9 @@ angular.module('dmtool', [
 		function ( $stateProvider,   $urlRouterProvider,   jwtInterceptorProvider, 	 $httpProvider, 	stateHelperProvider) {
 			$urlRouterProvider.otherwise("/");
 
-			stateHelperProvider
+			$stateProvider
 				//external - should redirect to adventure select if logged in
-				.state({
-					name: "root",
+				.state("root", {
 					url: "/",
 					data: {
 						requiresLogin: false
@@ -33,170 +32,168 @@ angular.module('dmtool', [
 							templateUrl: 'login_and_reg/login.html',
 							controller: 'LoginCtrl'
 						}
-					},
-					children: [
-						{
-							name: "register",
-					  	url: "register",
-					  	views: {
-								"content@" : {
-							    templateUrl: 'login_and_reg/register.html',
-									controller: 'RegisterCtrl'
-								}
-							}
-					  },
-					  {
-							name: "forgot_info",
-					  	url: "forgot_info",
-					  	views: {
-								"content@" : {
-							    templateUrl: 'login_and_reg/forgot_info.html',
-								}
-							}
-					  },
-					  {
-					  	name: "home",
-					  	url: "/home",
-					  	data: {
-					  		requiresLogin: true
-					  	},
-					  	views: {
-								"header@" : {
-									templateUrl: 'navbar/navbar_internal.html',
-									controller: 'NavbarCtrl'
-								},
-								"content@" : {
-							    templateUrl: 'adventure_select/adventure_select.html'
-								}
-							},
-					  	children: [
-							  {
-					  			name: "adventure_view",
-							  	url: "/adventure_view",
-							  	views: {
-							  		"content@" : {
-									    templateUrl: 'adventure_view/adventure_view.html',
-									    controller: 'AdventureViewCtrl'
-							  		}
-							  	},
-							  	children: [
-							    // 	{
-								  	// 	name: "dm",
-									  // 	url: "/dm",
-									  // 	views: {
-									  // 		"adv_body@" : {
-											//     templateUrl: 'adventure_view/pages/adventure.html',
-											//     controller: 'AdventureViewTabsCtrl'
-									  // 		}
-									  // 	}
-									  // },
-									  {
-									  	name: "player",
-									  	url: "/player",
-									  	views: {
-									  		"content@" : {
-											    templateUrl: 'adventure_view/pages/adventure.html',
-											    controller: function ($state, $scope, $window) {
-														console.log("hi");
-													}
-									  		}
-									  	}
-									  }
-							  	]
-							  },
-							  {
-					  			name: "create_char",
-							  	url: "/create_char",
-							  	views: {
-							  		"content@" : {
-							  			templateUrl: 'new_adventure/character_create.html'
-							  		}	
-							  	}
-							  },
-							  {
-					  			name: "roll_new_char",
-							  	url: "/roll_new_char",
-							  	views: {
-							  		"content@" : {
-									    templateUrl: 'new_adventure/roll_new_char.html'
-							  		}	
-							  	}
-							  }
-					  	]
-					  }
-					]
+					}
 				})
-			  //internal - should redirect to root if not logged in
-			  .state({
-			  	name: "home",
-			  	url: "/home",
-			  	data: {
-			  		requiresLogin: true
-			  	},
+				.state("register", {
+			  	url: "/register",
 			  	views: {
-						"header@" : {
-							templateUrl: 'navbar/navbar_internal.html',
-							controller: 'NavbarCtrl'
-						},
 						"content@" : {
-					    templateUrl: 'adventure_select/adventure_select.html'
+					    templateUrl: 'login_and_reg/register.html',
+							controller: 'RegisterCtrl'
 						}
-					},
-			  	children: [
-					  {
-			  			name: "adventure_view",
-					  	url: "/adventure_view",
-					  	views: {
-					  		"content@" : {
-							    templateUrl: 'adventure_view/adventure_view.html',
-							    controller: 'AdventureViewCtrl'
-					  		}
-					  	},
-					  	children: [
-					    // 	{
-						  	// 	name: "dm",
-							  // 	url: "/dm",
-							  // 	views: {
-							  // 		"adv_body@" : {
-									//     templateUrl: 'adventure_view/pages/adventure.html',
-									//     controller: 'AdventureViewTabsCtrl'
-							  // 		}
-							  // 	}
-							  // },
-							  {
-							  	name: "player",
-							  	url: "/player",
-							  	views: {
-							  		"adv@root.adventure_view" : {
-									    templateUrl: 'adventure_view/pages/adventure.html',
-									    controller: function ($state, $scope, $window) {
-												console.log("hi");
-											}
-							  		}
-							  	}
-							  }
-					  	]
-					  },
-					  {
-			  			name: "create_char",
-					  	url: "/create_char",
-					  	views: {
-					  		"content@" : {
-					  			templateUrl: 'new_adventure/character_create.html'
-					  		}	
-					  	}
-					  },
-					  {
-			  			name: "roll_new_char",
-					  	url: "/roll_new_char",
-					  	views: {
-					  		"content@" : {
-							    templateUrl: 'new_adventure/roll_new_char.html'
-					  		}	
-					  	}
-					  }
-			  	]
+					}
 			  })
+			  .state("forgot_info", {
+			  	url: "/forgot_info",
+			  	views: {
+						"content@" : {
+					    templateUrl: 'login_and_reg/forgot_info.html'
+						}
+					}
+			  })
+				// children: [
+				//   {
+				//   	name: "home",
+				//   	url: "/home",
+				//   	data: {
+				//   		requiresLogin: true
+				//   	},
+				//   	views: {
+				// 			"header@" : {
+				// 				templateUrl: 'navbar/navbar_internal.html',
+				// 				controller: 'NavbarCtrl'
+				// 			},
+				// 			"content@" : {
+				// 		    templateUrl: 'adventure_select/adventure_select.html'
+				// 			}
+				// 		},
+				//   	children: [
+				// 		  {
+				//   			name: "adventure_view",
+				// 		  	url: "/adventure_view",
+				// 		  	views: {
+				// 		  		"content@" : {
+				// 				    templateUrl: 'adventure_view/adventure_view.html',
+				// 				    controller: 'AdventureViewCtrl'
+				// 		  		}
+				// 		  	},
+				// 		  	children: [
+				// 		    // 	{
+				// 			  	// 	name: "dm",
+				// 				  // 	url: "/dm",
+				// 				  // 	views: {
+				// 				  // 		"adv_body@" : {
+				// 						//     templateUrl: 'adventure_view/pages/adventure.html',
+				// 						//     controller: 'AdventureViewTabsCtrl'
+				// 				  // 		}
+				// 				  // 	}
+				// 				  // },
+				// 				  {
+				// 				  	name: "player",
+				// 				  	url: "/player",
+				// 				  	views: {
+				// 				  		"content@" : {
+				// 						    templateUrl: 'adventure_view/pages/adventure.html',
+				// 						    controller: function ($state, $scope, $window) {
+				// 									console.log("hi");
+				// 								}
+				// 				  		}
+				// 				  	}
+				// 				  }
+				// 		  	]
+				// 		  },
+				// 		  {
+				//   			name: "create_char",
+				// 		  	url: "/create_char",
+				// 		  	views: {
+				// 		  		"content@" : {
+				// 		  			templateUrl: 'new_adventure/character_create.html'
+				// 		  		}	
+				// 		  	}
+				// 		  },
+				// 		  {
+				//   			name: "roll_new_char",
+				// 		  	url: "/roll_new_char",
+				// 		  	views: {
+				// 		  		"content@" : {
+				// 				    templateUrl: 'new_adventure/roll_new_char.html'
+				// 		  		}	
+				// 		  	}
+				// 		  }
+				//   	]
+				//   }
+				// ]
+			 //  //internal - should redirect to root if not logged in
+			 //  .state({
+			 //  	name: "home",
+			 //  	url: "/home",
+			 //  	data: {
+			 //  		requiresLogin: true
+			 //  	},
+			 //  	views: {
+				// 		"header@" : {
+				// 			templateUrl: 'navbar/navbar_internal.html',
+				// 			controller: 'NavbarCtrl'
+				// 		},
+				// 		"content@" : {
+				// 	    templateUrl: 'adventure_select/adventure_select.html'
+				// 		}
+				// 	},
+			 //  	children: [
+				// 	  {
+			 //  			name: "adventure_view",
+				// 	  	url: "/adventure_view",
+				// 	  	views: {
+				// 	  		"content@" : {
+				// 			    templateUrl: 'adventure_view/adventure_view.html',
+				// 			    controller: 'AdventureViewCtrl'
+				// 	  		}
+				// 	  	},
+				// 	  	children: [
+				// 	    // 	{
+				// 		  	// 	name: "dm",
+				// 			  // 	url: "/dm",
+				// 			  // 	views: {
+				// 			  // 		"adv_body@" : {
+				// 					//     templateUrl: 'adventure_view/pages/adventure.html',
+				// 					//     controller: 'AdventureViewTabsCtrl'
+				// 			  // 		}
+				// 			  // 	}
+				// 			  // },
+				// 			  {
+				// 			  	name: "player",
+				// 			  	url: "/player",
+				// 			  	views: {
+				// 			  		"adv@root.adventure_view" : {
+				// 					    templateUrl: 'adventure_view/pages/adventure.html',
+				// 					    controller: function ($state, $scope, $window) {
+				// 								console.log("hi");
+				// 							}
+				// 			  		}
+				// 			  	}
+				// 			  }
+				// 	  	]
+				// 	  },
+				// 	  {
+			 //  			name: "create_char",
+				// 	  	url: "/create_char",
+				// 	  	views: {
+				// 	  		"content@" : {
+				// 	  			templateUrl: 'new_adventure/character_create.html'
+				// 	  		}	
+				// 	  	}
+				// 	  },
+				// 	  {
+			 //  			name: "roll_new_char",
+				// 	  	url: "/roll_new_char",
+				// 	  	views: {
+				// 	  		"content@" : {
+				// 			    templateUrl: 'new_adventure/roll_new_char.html'
+				// 	  		}	
+				// 	  	}
+				// 	  }
+			 //  	]
+			 //  })
 
 			  ;
 		}
