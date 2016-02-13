0293ff604e331e5fc91bf93302f2bc581d0d3737
Ryan <ryantang333@gmail.com>
2016-02-13 08:03:49 -0800

    removing builds

--- client/adventure-select/adventure-select.controller.js
+++ client/adventure-select/adventure-select.controller.js
@@ -31,8 +31,8 @@
       var data = vm.new_adventure;
       data.user_id = vm.me._id;
       adventureFactory.create(data)
-        .then(function(data) {
-          vm.adventures.push(data);
+        .then(function(res) {
+          vm.adventures.push(res);
           vm.new_adventure = null;
         });
     }
